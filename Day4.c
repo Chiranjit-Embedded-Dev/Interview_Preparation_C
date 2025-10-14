bfd1f75d43712a89862cd4d4bfebd39ed57b3ebe
Chiranjit Adhikary <chiranjitadhikary.info@gmail.com>
2025-10-14 23:52:59 +0530

    Signed-off-by: Chiranjit Adhikary <chiranjitadhikary.info@gmail.com>

--- Day4.c
+++ Day4.c
@@ -1,16 +1,30 @@
 // Recursive Functions
-#include <stdio.h>
-int fun(int n)
-{
-    if (n == 1)
-        return 1;
-    else
-        return 7 + fun(n - 2);
-}
-int main()
-{
+// #include <stdio.h>
+// int fun(int n)
+// {
+//     if (n == 1)
+//         return 1;
+//     else
+//         return 1 + fun(n - 1);
+// }
+// int main()
+// {
 
-    // int n =4;
-    printf(" %d", fun(7));
+//     // int n =4;
+//     printf(" %d", fun(4));
+//     return 0;
+// }
+#include <stdio.h>
+
+void printNumbers(int n) {
+    if (n == 0) // base case
+        return;
+    printNumbers(n - 1); // recursive call
+    printf("%d ", n);    // print after recursion to get 1..N
+}
+
+int main() {
+    int n = 10;
+    printNumbers(n);
     return 0;
 }
