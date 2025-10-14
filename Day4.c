e1d50b37487e15850cb5fd72b921a2f631ec232c
Chiranjit Adhikary <chiranjitadhikary.info@gmail.com>
2025-10-14 23:38:44 +0530

    Signed-off-by: Chiranjit Adhikary <chiranjitadhikary.info@gmail.com>

--- Day4.c
+++ Day4.c
@@ -1,9 +1,16 @@
 // Recursive Functions
 #include <stdio.h>
+int fun(int n)
+{
+    if (n == 1)
+        return 1;
+    else
+        return 7 + fun(n - 2);
+}
 int main()
 {
-    
-    
+
+    // int n =4;
+    printf(" %d", fun(7));
     return 0;
-    
 }
