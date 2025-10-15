dcd1bc607a0f3e389b2efb56662652df41dfc112
Chiranjit Adhikary <chiranjitadhikary.info@gmail.com>
2025-10-15 22:12:53 +0530

    Signed-off-by: Chiranjit Adhikary <chiranjitadhikary.info@gmail.com>

--- Day5.c
+++ Day5.c
@@ -1,1 +1,22 @@
-//RE
+// Recursion in C 
+#include<stdio.h>
+
+int fun_factroal(int n)
+{
+    if (n==0)
+    {
+        return 1;
+    }
+    else
+    return n*fun_factroal(n-1);
+
+}
+int main()
+{
+    int number;
+    printf("Plesae enter a number that you want to do factorial =");
+    scanf("%d",&number);
+    fun_factroal(number);
+    printf("factorial of number of %d =%d",number,fun_factroal(number));
+    return 0;
+}
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,1 +1,24 @@
-void
+// Recursion in C
+#include <stdio.h>
+
+int fun_factroal(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+        return n * fun_factroal(n - 1);
+}
+int main()
+{
+    int number;
+    printf("Plesae enter a number that you want to do factorial =");
+    scanf("%d", &number);
+    // fun_factroal(number);
+    // printf("Factorial of number of %d =%d",number,fun_factroal(number));
+    // can be also do by this
+    int result = fun_factroal(number);
+    printf("Factorial of number of %d =%d", number, result);
+    return 0;
+}
