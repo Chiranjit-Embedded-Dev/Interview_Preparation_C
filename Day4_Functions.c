73201bfbf9552157f7005ba5178a2d782326b64a
Chiranjit-Embedded-Dev <chiranjitrock007@gmail.com>
2025-10-18 22:00:16 +0530

    Signed-off-by: Chiranjit-Embedded-Dev <chiranjitrock007@gmail.com>

---
+++ Day4_Functions.c
@@ -0,0 +1,22 @@
+// Function
+
+//#include <stdio.h>
+
+// int fun()
+// {
+//     for (int i = 0; i < 5; i++)
+//     {
+//         printf("\n*");
+//     }
+
+//     return 0;
+// }
+
+// int main()
+// {
+//     fun();
+//     printf("Hallo World");
+//     return 0;
+// }
+
+
