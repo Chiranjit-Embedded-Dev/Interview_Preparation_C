95e68d2fcae90a3ad53c6aa997c5408cba217733
Chiranjit-Embedded-Dev <chiranjitrock007@gmail.com>
2025-10-22 21:40:45 +0530

    Signed-off-by: Chiranjit-Embedded-Dev <chiranjitrock007@gmail.com>

--- Day3_Loop.c
+++ Day3_Loop.c
@@ -11,19 +11,48 @@
 //     }
 //     return 0;
 // }
+//
+
+// Print Even Number and odd number
+
+// #include <stdio.h>
+// int main()
+
+// {
+//     for (int i = 1; i <= 100; i++)
+//     {
+//         if (i % 2 != 0)
+//         {
+//             printf("Print Even Number is= %d\n",i);
+//         }
+//     }
+//       return 0;
+// }
+
+// print even number
 
 #include <stdio.h>
 int main()
-
 {
-    for (int i = 1; i <= 100; i++)
-    {
-        if (i % 2 != 0)
-        {
-            printf("Print Even Number is= %d\n",i);
-        }
 
-      
+    int num;
+    int flag = 0;
+    printf("Please enter a number =");
+    scanf("%d", &num);
+
+    for (int i = 2; i <= num / 2; i++)
+    {
+        if (num % i == 0)
+        {
+            flag = 1;
+        }
     }
-      return 0;
+
+    if (flag == 1)
+    {
+        printf("Non Prime Number");
+    }
+    else
+        printf("Prime Namber");
+    return 0;
 }
