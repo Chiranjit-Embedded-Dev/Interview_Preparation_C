f1edda6873a372c501890920f49250399f963804
Chiranjit-Embedded-Dev <chiranjitrock007@gmail.com>
2025-10-29 00:07:00 +0530

    Signed-off-by: Chiranjit-Embedded-Dev <chiranjitrock007@gmail.com>

---
+++ Day11_File_Handling.c
@@ -0,0 +1,54 @@
+// // 
+// What is File Handling in C?
+
+// File handling allows your program to 
+// store data permanently on a disk instead of just using RAM.
+// Let’s understand why files are needed in C
+
+// 1. Memory in C is Temporary
+
+// When you run a C program, all variables (like int, char, float, arrays, etc.) are stored in RAM (volatile memory).
+// When the program finishes or your system restarts → all data is lost.
+
+
+// 2. Files Allow Permanent Data Storage
+// Files store data permanently on disk (like hard drive, SSD, SD card, or flash memory).
+// Even after the program ends or power is off, the data remains saved.
+
+// 👉 Example use cases:
+
+// Saving user input, sensor logs, or device configuration.
+
+// Keeping high scores in a game.
+
+// Storing student records in a database.
+
+// Logging system events or errors.
+
+// Creat a file in c
+
+#include <stdio.h>
+
+int main() {
+    FILE *fp;  // File pointer
+
+    // Open file for writing (create if not exist)
+    fp = fopen("myfile.txt", "w");
+
+    // Check if file opened successfully
+    if (fp == NULL) {
+        printf("Error creating file!\n");
+        return 1;
+    }
+
+    // Write data to file
+    fprintf(fp, "Hello, this is a new file created using C program_1.\n");
+    fprintf(fp, "File handling is easy once you understand it!\n");
+
+    // Close file
+    fclose(fp);
+
+    printf("File created and data written successfully.\n");
+
+    return 0;
+}
