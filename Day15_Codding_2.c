0ee93911d146318bd247eb76d496d5d64c6cee60
Chiranjit-Embedded-Dev <chiranjitrock007@gmail.com>
2025-11-05 23:07:13 +0530

    Signed-off-by: Chiranjit-Embedded-Dev <chiranjitrock007@gmail.com>

--- Day15_Codding_2.c
+++ Day15_Codding_2.c
@@ -1,17 +1,40 @@
 // Calcluute sum of two number
+// #include <stdio.h>
+// int fun_sum()
+// {
+//     int a = 0;
+//     int b = 0;
+//     printf("Pleae enter value of A And B\n");
+//     scanf("%d %d", &a,&b);
+//     int sum = a + b;
+//     printf("Sum of A-%d and B-%d =%d", a, b, sum);
+//     return 0;
+// }
+// int main()
+// {
+//     fun_sum();
+//     return 0;
+// }
+//
+
+// Implement a function to reverse a string in C
 #include <stdio.h>
-int fun_sum()
+#include <string.h>
+
+void str_reverse()
 {
-    int a = 0;
-    int b = 0;
-    printf("Pleae enter value of A And B\n");
-    scanf("%d %d", &a,&b);
-    int sum = a + b;
-    printf("Sum of A-%d and B-%d =%d", a, b, sum);
-    return 0;
+    char str[10] = {"Hello"};
+    int len = strlen(str);
+    for (int i = len - 1; i >= 0; i--)
+        
+    {
+        printf("%c", str[i]);
+    }
+    printf("\n");
 }
+
 int main()
 {
-    fun_sum();
+    str_reverse();
     return 0;
 }
