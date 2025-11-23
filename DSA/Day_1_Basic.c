3d36d26505eed0d12baa3149d45418b392c52cc5
Chiranjit-Embedded-Dev <chiranjitrock007@gmail.com>
2025-11-24 00:01:40 +0530

    Signed-off-by: Chiranjit-Embedded-Dev <chiranjitrock007@gmail.com>

---
+++ DSA/Day_1_Basic.c
@@ -0,0 +1,107 @@
+// // ✅ 1. Basic Foundations (Must Know Before DSA)
+// ✔ C fundamentals
+
+// Pointers
+
+// Arrays
+
+// Functions
+
+// Structures
+
+// Memory layout (stack/heap)
+
+// Time & Space complexity basics
+
+
+
+// ✅ 2. Core Data Structures (DSA Heart)
+
+// 🔹 Arrays
+
+// Searching (Linear, Binary Search)
+
+// Sorting basics (Bubble, Selection, Insertion)
+
+// 🔹 Strings
+
+// Palindrome
+
+// Reverse
+
+// Substring matching
+
+// 🔹 Linked List
+
+// Singly
+
+// Doubly
+
+// Circular
+
+// Operations: insert, delete, reverse
+
+// 🔹 Stack
+
+// Array implementation
+
+// Linked list implementation
+
+// Applications:
+
+// Parenthesis matching
+
+// Infix → Postfix
+
+// Undo operations
+
+// 🔹 Queue
+
+// Normal queue
+
+// Circular queue
+
+// Priority queue
+
+// Deque
+
+// 🔹 Trees
+
+// Binary tree
+
+// BST (Binary Search Tree)
+
+// Traversals (Inorder, Preorder, Postorder)
+
+// Height, Level-order
+
+// Insert/Delete in BST
+
+// 🔹 Heaps
+
+// Min Heap
+
+// Max Heap
+
+// Heapify
+
+// Heap Sort
+
+// 🔹 Hashing
+
+// Hash functions
+
+// Collision handling (chaining, open addressing)
+
+// 🔹 Graphs
+
+// Representations (Adj. list / matrix)
+
+// BFS
+
+// DFS
+
+// Shortest path (Dijkstra)
+
+// MST (Kruskal / Prim)
+
